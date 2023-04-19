0ed439cd1a9dee5674a6e358b454333f10fd5c24
LucasParra95 <lucasandresparra@gmail.com>
2023-04-18 21:07:30 -0300

    https fetch

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,7 +9,7 @@ export default function Home() {
   const apiKey = process.env.REACT_APP_API_KEY;
 
   function onSearch(ciudad) {
-    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${ciudad}&appid=${apiKey}&units=metric`)
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${ciudad}&appid=${apiKey}&units=metric`)
       .then(r => r.json())
       .then((recurso) => {
         if(recurso.main !== undefined){
